0e67d1570498d4feaa56559d453c8e882783245d
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-12-11 23:39:36 +0600

    project api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,11 +14,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const projectCollection = client.db("myPortfolio").collection("projects");
+
+    app.get("/projects", async (req, res) => {
+      const query = {};
+      const cursor = projectCollection.find(query);
+      const projects = await cursor.toArray();
+      res.send(projects);
+    });
+
+    app.get("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const project = await projectCollection.findOne(query);
+      res.send(project);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("portfolio server running");
